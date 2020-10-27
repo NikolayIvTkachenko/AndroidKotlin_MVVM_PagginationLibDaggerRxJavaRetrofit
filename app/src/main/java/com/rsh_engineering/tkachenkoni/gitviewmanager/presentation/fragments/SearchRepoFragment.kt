8b41128adb36f067cdd46b171ed13b3df156c430
Nikolay <nickolay.i.tkachenko@gmail.com>
2020-10-27 18:27:20 +0300

    v 12

--- app/src/main/java/com/rsh_engineering/tkachenkoni/gitviewmanager/presentation/fragments/SearchRepoFragment.kt
+++ app/src/main/java/com/rsh_engineering/tkachenkoni/gitviewmanager/presentation/fragments/SearchRepoFragment.kt
@@ -1,7 +1,6 @@
 package com.rsh_engineering.tkachenkoni.gitviewmanager.presentation.fragments
 
 import android.content.Context
-import android.opengl.Visibility
 import android.os.Bundle
 import android.util.Log
 import android.view.View
@@ -12,15 +11,12 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.rsh_engineering.tkachenkoni.gitviewmanager.App
 import com.rsh_engineering.tkachenkoni.gitviewmanager.R
 import com.rsh_engineering.tkachenkoni.gitviewmanager.data.repository_impl.NetworkState
-import com.rsh_engineering.tkachenkoni.gitviewmanager.domain.model_entity.ItemResponse
-import com.rsh_engineering.tkachenkoni.gitviewmanager.domain.model_entity.SearchResponse
 import com.rsh_engineering.tkachenkoni.gitviewmanager.presentation.adapters.RepoListAdapter
 import com.rsh_engineering.tkachenkoni.gitviewmanager.presentation.utils.hideKeyboard
 import com.rsh_engineering.tkachenkoni.gitviewmanager.presentation.viewmodels.GeneralViewModel
 import com.rsh_engineering.tkachenkoni.gitviewmanager.presentation.viewmodels.SearcViewModel
 import com.rsh_engineering.tkachenkoni.gitviewmanager.presentation.viewmodels.ViewModelFactory
 import kotlinx.android.synthetic.main.fragment_search_repo.*
-import kotlinx.android.synthetic.main.network_state_item.*
 import javax.inject.Inject
 
 
@@ -33,6 +29,8 @@ class SearchRepoFragment : BaseFragment() {
 
     @Inject
     lateinit var viewModelFactory: ViewModelFactory
+
+    private var bntPressedCallBack = false
 
     override fun getLayoutById(): Int {
         return R.layout.fragment_search_repo
@@ -66,9 +64,25 @@ class SearchRepoFragment : BaseFragment() {
         }
 
         btn_search.setOnClickListener {
-            if(et_input_search.text != null && et_input_search.text.length > 0){
-                updateAfterClickButtonFind()
+            if (!bntPressedCallBack){
+                requestDataRepositories()
             }
+        }
+
+//        RxTextView.textChanges(et_input_search)
+//            .debounce(700, TimeUnit.MILLISECONDS)
+//            .subscribe(object : Action1<CharSequence> {
+//                override fun call(t: CharSequence?) {
+//                    requestDataRepositories()
+//                }
+//            })
+    }
+
+    fun requestDataRepositories(){
+        if(et_input_search.text != null && et_input_search.text.length > 0){
+            updateAfterClickButtonFind()
+            bntPressedCallBack = true
+            btn_search.isEnabled = false
         }
     }
 
@@ -87,32 +101,48 @@ class SearchRepoFragment : BaseFragment() {
 
     fun networkStatus(){
         generalViewModel.getNetworkState().observe(viewLifecycleOwner, Observer {
-            sf_progress_bar.visibility = if (generalViewModel.listIsEmpty() && it == NetworkState.LOADING) View.VISIBLE else View.GONE
-            sf_txt_error.visibility = if (generalViewModel.listIsEmpty() && it == NetworkState.ERROR) View.VISIBLE else View.GONE
 
-            if(!generalViewModel.listIsEmpty()){
+            bntPressedCallBack = false
+            btn_search.isEnabled = true
+
+            sf_progress_bar.visibility =
+                if (generalViewModel.listIsEmpty() && it == NetworkState.LOADING) View.VISIBLE else View.GONE
+            sf_txt_error.visibility =
+                if (generalViewModel.listIsEmpty() && it == NetworkState.ERROR) View.VISIBLE else View.GONE
+
+            if (!generalViewModel.listIsEmpty()) {
                 repoadapter.setNetworkState(it)
                 repoadapter.setSearchText(et_input_search.text.toString())
             }
 
-            when(it){
-                NetworkState.LOADING -> {tv_message.text = NetworkState.LOADING.msg}
-                NetworkState.ERROR -> {tv_message.text = NetworkState.ERROR.msg}
-                NetworkState.LOADED -> {tv_message.text = NetworkState.LOADED.msg}
-                NetworkState.ENDOFLIST -> {tv_message.text = NetworkState.ENDOFLIST.msg}
+            when (it) {
+                NetworkState.LOADING -> {
+                    tv_message.text = NetworkState.LOADING.msg
+                }
+                NetworkState.ERROR -> {
+                    tv_message.text = NetworkState.ERROR.msg
+                }
+                NetworkState.LOADED -> {
+                    tv_message.text = NetworkState.LOADED.msg
+                }
+                NetworkState.ENDOFLIST -> {
+                    tv_message.text = NetworkState.ENDOFLIST.msg
+                }
             }
         })
     }
 
     fun searchText(){
-        generalViewModel.itemResponsePageList.observe(viewLifecycleOwner, Observer {pageList ->
-            if(pageList!=null) {
+        generalViewModel.itemResponsePageList.observe(viewLifecycleOwner, Observer { pageList ->
+            bntPressedCallBack = false
+            btn_search.isEnabled = true
+            if (pageList != null) {
                 Log.d("TESTNETWORK", "pageList!=null")
                 sf_progress_bar.visibility = View.GONE
                 sf_txt_error.visibility = View.GONE
 
                 repoadapter.submitList(pageList)
-            }else{
+            } else {
                 Log.d("TESTNETWORK", "pageList==null")
             }
         })
